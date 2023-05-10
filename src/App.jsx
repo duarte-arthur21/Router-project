8dd2fad80be8341a5e9e537da5fb2ee3b92eb2ff
duarte-arthur21 <ferreiraduarte21@gmail.com>
2023-05-10 14:42:57 -0300

    Comentando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { MyRoutes } from "./routes";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      {/* Componente Router é usado para monitorar as rotas */}
 
-export default App
+      <div>
+        {/* Cria omenu de navegação para os componentes atraves de event onClick */}
+
+        {/* Componente Menu */}
+        <nav>
+          <ul>
+            <li>
+              {/* Passa o paramentro "/" para o componente MyRoutes */}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              {/* Passa o paramentro "/about" para o componente MyRoutes */}
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <MyRoutes />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
