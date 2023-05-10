1bb5d02be6bbbaabffbb85ccd3b5c9eb1faef2c6
duarte-arthur21 <ferreiraduarte21@gmail.com>
2023-05-10 14:43:46 -0300

    comentando

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,11 @@ import { About } from "./pages/About";
 const MyRoutes = () => {
   return (
     <Routes>
+      {/* Routes é o componetes usado para criar rotas */}
+
+      {/* Cria a rota '/' para o componente Home */}
       <Route exact path="/" element={<Home />} />
+      {/* Cria a rota '/about' para o componente About */}
       <Route path="/about" element={<About />} />
     </Routes>
   );
